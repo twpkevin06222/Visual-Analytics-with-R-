f10613944f0dc862cebad0b94b9a99b230de2b51
Wai Po Kevin Teng <37227666+twpkevin06222@users.noreply.github.com>
2018-06-09 17:21:34 +0200

    Update app.R

--- Code/Project2/app.R
+++ Code/Project2/app.R
@@ -42,7 +42,16 @@ ui <- dashboardPage(
       ),
       box(
         title='Table with respect to Cluster', status = "primary", solidHeader = TRUE,
-        dataTableOutput('clusterTable')
+        dataTableOutput('clusterTable'),
+        plotOutput('dt_plot_c')
+      )
+    ),
+    titlePanel("Third Assignment"),
+    fluidRow(
+      box(
+        title = "Characteristic of Clustering", status = "primary", solidHeader = TRUE,
+        sliderInput("k03Input","Number of Cluster:", min=1,max=10, value = c(5)),
+        plotOutput("char01_plot")
       )
     )
   )
@@ -89,6 +98,46 @@ dt_c <- reactive({
   return(medoids)
 })
 
+#3---------------------------------------------------------------------------------
+char01 <- reactive({
+  m1 <- count(data,forename) #count occurence of forename throughout the dataset
+  m2 <- m1[!rowSums(m1[2]<5),]  #delete row less than 5 in m1
+  m5 <- merge(m2,data,by.x='forename')
+  m6 <- expand.grid(forename=unique(m5$forename), n = unique(m5$n), sex = unique(m5$sex))
+  m7 <- merge(m5,m6, all = TRUE)
+  m7$rank[is.na(m7$rank)] <- 0  #convert na = 0
+  m7$popularity <- ifelse(m7$rank==0, yes=0,no=1-(m7$rank-min(m7$rank))/(max(m7$rank)-min(m7$rank)))
+  m11 <- aggregate(popularity~forename+year,m7,sum)
+  m12 <- spread(m11,year,popularity)
+  m13 <- m12
+  forename <- m12$forename
+  m13[is.na(m13)] <- 0 #convert na to 0
+  medoids <- m13
+  medoids$forename <- NULL #change column name to null
+  rownames(medoids) <- forename
+  pam.result <- pam(medoids,input$k03Input)
+  datalist=list()
+  for(i in 1:input$k03Input){
+    t1 <- data.frame(pam.result$clustering)
+    t2 <- setDT(t1,keep.rownames=TRUE)[]
+    colnames(t2)[1] <- "forename"
+    t3 <- merge(t2,m11,by='forename', all=TRUE)
+    t4 <- t3 %>% filter(pam.result.clustering==i)
+    t1111 <- expand.grid(forename=unique(t4$forename),year=unique(t4$year))
+    t1112 <- merge(t4,t1111,all=TRUE)
+    t1112$popularity[is.na(t1112$popularity)] <- 0 
+    t1113 <- count(t1112,year,popularity)
+    t1114 <- aggregate(popularity~year,t1113,sum)
+    t1115 <- count(t1112,year)
+    t1116 <- merge(t1114,t1115,by='year')
+    t1116$avg_popularity <- t1116[,2]/t1116[,3]
+    t1116$cluster <- i
+    datalist[[i]] <- t1116
+  }
+  big_data = do.call(rbind,datalist)
+  return(big_data)
+})
+
   #Main Output---------------------------------------------------------------------
   output$k_cluster <- renderPlot({
   if(input$yesnoInput=='no'){
@@ -100,7 +149,7 @@ dt_c <- reactive({
   #2-------------------
   output$s_plot <- renderPlot({
     if(input$radio_yes_noInput=='Auto'){
-      fviz_nbclust(medoids,pam,k.max=20,method='silhouette')
+      fviz_nbclust(dt_c(),pam,k.max=20,method='silhouette')
     }
   })
   #2 Table-----------------------------------------------------
@@ -113,7 +162,22 @@ dt_c <- reactive({
     df <- data.frame(pam.result$clustering)
     datatable(df, colnames=c('forename','clustering'))
   })
+  output$dt_plot_c <- renderPlot({
+    s <- input$clusterTable_rows_selected
+    if(input$radio_yes_noInput=='Auto'){
+      pam.result <- pam(dt_c(),k=2)
+    }else{
+      pam.result <- pam(dt_c(),input$k02Input)  
+    }
+    clusplot(pam.result, main='Scatterplot for Clustering')
+    df <- data.frame(pam.result$clustering)
+    if (length(s)) points(df[s, , drop = FALSE], pch = 19, cex = 2)
+  })
   
+  #3 Plot---------------------------------------------------------
+  output$char01_plot <- renderPlot({
+    ggplot(char01(),aes(x=year,y=avg_popularity,group=cluster, colour= cluster))+geom_line()
+  })
 }
 #End
 #----------------------------------------------------------------------
